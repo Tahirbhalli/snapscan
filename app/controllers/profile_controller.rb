476ff65ec03bd7528e043162fa6fabe66776215d
tahirbhalli <tahirbhalli16@gmail.com>
2020-05-27 17:28:04 +0500

    complete authentication

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,9 @@
 class ProfileController < ApplicationController
+    before_action :check_session
+    def index
+    end
+    private
+    def check_session
+        redirect_to user_index_path if session[:current_user] == ""
+    end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,17 @@
 class UserController < ApplicationController
+    before_action :check_session
   def index; end
 
+  def def new
+      @user = User.new
+  end
+  def create
+    @user=User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
+    create_session(@user.id,[:name],params[:password])
+    redirect_to profile_index_path
+  end
+  
   def login
     redirect_to home_user_path(session[:current_user]) if isuser?
   end
@@ -8,15 +19,18 @@ class UserController < ApplicationController
   def auth
     if User.exists?(email: params[:email], password: params[:password])
       id = User.where(email: params[:email], password: params[:password])
-      session[:current_user] = id.ids[0]
-      session[:user_name]=id.name
-      session[:user_email]=id.email
+      
+      create_session(id[0].id,id[0].name,id[0].email)
       redirect_to home_user_path(id.ids[0])
     end
-    render :login
   end
 
   private
+  def create_session(id,name,email)
+    session[:current_user]=id
+    session[:user_email]=email
+    session[:user_name]=name
+  end 
 
   def login_params
     params.require(:user).permit(:email, :password)
@@ -26,5 +40,8 @@ class UserController < ApplicationController
     return false if session[:current_user] == ''
 
     true
+  end
+  def check_session
+    redirect_to profile_index_path if session[:current_user] != ""
   end
 end
