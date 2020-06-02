ece6fcafb60c300ea81101b63fa9e5e017c86514
tahirbhalli <tahirbhalli16@gmail.com>
2020-06-02 17:10:39 +0500

    rubocop

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -28,14 +28,15 @@ class ProfileController < ApplicationController
   def transactions
     @tran = User.find(session[:current_user]).transactions
   end
+
   def external
     @external = User.find(session[:current_user]).transactions
   end
+
   def groups
-    @groups=Group.all
-    #@curent=User.find(session[:current_user])
-    #@groups=@curent.groups
-    
+    @groups = Group.all
+    # @curent=User.find(session[:current_user])
+    # @groups=@curent.groups
   end
 
   def newgroup; end
